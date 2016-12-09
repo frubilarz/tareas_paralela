58bcfee509aa35c6fd368d8ad288b52f1ac47506
NataliaFaundez <natalia.faundez@ug.uchile.cl>
2016-12-08 23:16:36 -0300

    problemas con array_split, no estoy muy segura

--- tarea3/tarea3.py
+++ tarea3/tarea3.py
@@ -16,11 +16,12 @@ def diferencias(lista, promedio):
 
 if rank == 0:
 	lista= numpy.random.uniform(0,1,100) #devuelve una lista de 100 datos con distribucion de 0 a 1
-	print "la lista inicial es: "+ str(lista)  
+	print "la lista inicial es: "+ str(lista) 
+	lista_par=array_split(lista, size) #Divida la lista dada en la cantidad de procesadores 
 else:
 	lista= None
 
-lista_par=array_split(lista, size) #Divida la lista dada en la cantidad de procesadores
+
 repa= comm.scatter(lista_par,root=0) #hace el scatter que consiste en repartir la listan entre los procesadores
 for i in range(0,size):
     if rank==i:    	
@@ -29,8 +30,7 @@ for i in range(0,size):
         repartir=comm.gather(promedio, root=0) #recoge las listas repartidad de cada procesador y la devuelve al madre
 
 if rank==0:
-	print "lista : " +str(data.get('lista'))
-        
+	print "lista : " +str(data.get('lista'))        
     t1 = time.time()
     print "el tiempo de ejecucion es ",t1-t0
 
